f2758efe9ccad31fb2a74447062164bd0f3b57b7
Artem Redia <artem.redia@gmail.com>
2024-12-14 16:30:14 +0400

    chore: add formatting code tools

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	singleQuote: true,
+	trailingComma: 'all',
+	printWidth: 100,
+	jsxSingleQuote: true,
+	useTabs: false,
+	tabWidth: 2,
+	bracketSameLine: true,
+	semi: false,
+	bracketSpacing: true,
+	endOfLine: 'lf',
+}
