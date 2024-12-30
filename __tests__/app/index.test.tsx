c96c13486d491a820c7783672624dc09cf626f9c
Artem Redia <artem.redia@gmail.com>
2024-12-30 15:50:25 +0400

    feat: implement sign in

--- __tests__/app/index.test.tsx
+++ __tests__/app/index.test.tsx
@@ -1,13 +1,13 @@
 import { fireEvent, renderRouter } from 'expo-router/testing-library'
 import App from '@app/_layout'
-import SignInScreen from '@app/(auth)/sign-in'
+import SignInScreen, { DEFAULT_CREDENTIALS } from '@app/(auth)/sign-in'
 import HomeScreen from '@app/(main)/(tabs)/(homeTab)/home'
 import Providers from '@tests/helpers/Providers'
 
 import 'jest-styled-components'
 
-test('navigates to main app correctly', () => {
-  const { getByText, getPathname } = renderRouter(
+test('show error in case of missing username', () => {
+  const { getByText, getByTestId } = renderRouter(
     {
       index: App,
       '/sign-in': () => (
@@ -26,8 +26,38 @@ test('navigates to main app correctly', () => {
     },
   )
 
-  const button = getByText('Go to App')
+  const button = getByTestId('submit-btn')
   fireEvent.press(button)
+
+  const error = getByText('Username is required')
+  expect(error).toBeTruthy()
+})
+
+test('mocks input credentials and submits the form', () => {
+  const { getByTestId, getPathname } = renderRouter(
+    {
+      index: App,
+      '/sign-in': () => (
+        <Providers>
+          <SignInScreen />
+        </Providers>
+      ),
+      '/home': () => (
+        <Providers>
+          <HomeScreen />
+        </Providers>
+      ),
+    },
+    {
+      initialUrl: 'sign-in',
+    },
+  )
+
+  fireEvent.changeText(getByTestId('username-input'), DEFAULT_CREDENTIALS.name)
+
+  fireEvent.changeText(getByTestId('password-input'), DEFAULT_CREDENTIALS.password)
+
+  fireEvent.press(getByTestId('submit-btn'))
 
   expect(getPathname()).toBe('/home')
 })
