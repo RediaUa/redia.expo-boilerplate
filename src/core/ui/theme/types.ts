1f70a30a0600340f0bd62c5257284371258692f6
Artem Redia <artem.redia@gmail.com>
2024-12-30 13:44:59 +0400

    feat: add basic ui components

--- src/core/ui/theme/types.ts
+++ src/core/ui/theme/types.ts
@@ -11,6 +11,20 @@ type ColorMap = {
   error: string
   primary: string
   primaryFocused: string
+  primaryDisabled: string
+  toggles: {
+    on: {
+      background: string
+      thumb: string
+    }
+    off: {
+      background: string
+      thumb: string
+    }
+  }
+  buttons: {
+    text: string
+  }
 }
 
 type FontSizeMap = {
