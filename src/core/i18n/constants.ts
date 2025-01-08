7ab11fa8ecf2a5a5535cab456d838e97457ebdd8
Artem Redia <artem.redia@gmail.com>
2025-01-08 11:10:52 +0400

    feat: add more languages support

--- src/core/i18n/constants.ts
+++ src/core/i18n/constants.ts
@@ -1,6 +1,9 @@
 export enum SupportedLanguages {
-  EN = 'en',
+  EN = 'en-US', // English (USA)
   UK = 'uk', // Ukrainian
+  ES = 'es', // Spanish
+  AR = 'ar', // Arabic
+  JA = 'ja', // Japanese
 }
 
 export const DEFAULT_LANGUAGE = SupportedLanguages.EN
