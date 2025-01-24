518a54a5b882e16ea61325cc3b1f6d53ca673274
Artem Redia <artem.redia@gmail.com>
2025-01-24 14:50:04 +0400

    fix: remove screen container insets

--- src/core/ui/components/containers/ScreenContainer.tsx
+++ src/core/ui/components/containers/ScreenContainer.tsx
@@ -1,15 +1,21 @@
 import { FC, ReactNode } from 'react'
 import { ScrollView, ScrollViewProps, StyleProp, ViewStyle } from 'react-native'
-// hooks
 import { EdgeInsets, useSafeAreaInsets } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
 import styled from 'styled-components/native'
-// types
+
 type Props = ScrollViewProps & {
   children: ReactNode
   scrollable?: boolean
   style?: StyleProp<ViewStyle>
   contentContainerStyle?: StyleProp<ViewStyle>
+}
+
+const DEFAULT_INSETS = {
+  top: 0,
+  bottom: 16,
+  left: 0,
+  right: 0,
 }
 
 const ScreenContainer: FC<Props> = ({
@@ -19,10 +25,15 @@ const ScreenContainer: FC<Props> = ({
   contentContainerStyle,
   ...rest
 }) => {
+  /*
+      React Navigation handles safe area in the default header
+      if you're using a custom header, it's important to ensure your UI is within the safe area  
+      https://reactnavigation.org/docs/handling-safe-area
+  */
   const insets = useSafeAreaInsets()
 
   const content = (
-    <Container testID='screen-container' style={style} {...insets}>
+    <Container testID='screen-container' style={style} {...(DEFAULT_INSETS || insets)}>
       {children}
       <StatusBar style='auto' />
     </Container>
@@ -55,4 +66,6 @@ const Container = styled.View<EdgeInsets>`
   flex: 1;
   padding: ${({ left, right, top }) => `${top}px ${left}px ${right}px`};
   align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.background};
 `
